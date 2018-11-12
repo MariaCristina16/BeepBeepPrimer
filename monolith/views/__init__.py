1eeddc5ee73937352c909111aa184f44faf0180e
bongi23 <andrea.bongi@live.it>
2018-11-12 14:05:20 +0100

    init fix

--- monolith/views/__init__.py
+++ monolith/views/__init__.py
@@ -1,11 +1,10 @@
 from .home import home
 from .auth import auth
 from .users import users
-from .strava import strava
 from .objectives import objectives
 from .run import run
 from .statistics import statistics
 from .challenge import challenge
 from .report import report
 
-blueprints = [home, auth, users, strava, run, statistics, objectives, challenge, report]
+blueprints = [home, auth, users, run, statistics, objectives, challenge, report]
